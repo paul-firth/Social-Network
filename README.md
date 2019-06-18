79f467aa3e18a948ed3db4347e7360b85eaf2c15
paulfirth <paul.firth@griffithuni.edu.au>
2019-06-18 02:14:28 +0000

    Fixed issue with Guest users being unable to view profiles

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ Assignment 2 for Web Aplication Development
 
 A simple social network site named "Social Pandas".
 
-Built in Laravel and using SQLite, the site allow user's to register, login, make/edi/delete posts, see and make/delete comments on other posts, 
+Built in Laravel and using SQLite, the site allow user's to register, login, make/edit/delete posts, see and make/delete comments on other posts, 
 search users and add/remove friends.
+Comments on posts use pagenation to limit 6 comments per page.
 Friends are able to see each other's post list on their profile pages
 
 
--- ass2/resources/views/pages/profile.blade.php
+++ ass2/resources/views/pages/profile.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.master')
 
 @section('makepost')
-
+@if (Auth::guest()) 
     <div class="panel-heading"> 
         <img src="../{{$user->image}}" style="width:70px;height:70px;">
         <br>
-        <h2>{{$user->name}}</h2>
+        <h2>{{$user->name}}</h2> 
+    </div>
+    <div class="panel-body"> 
+            <p>{{$user->name}}'s Profile Page</p>
+    </div>
+@else
+    <div class="panel-heading"> 
+        <img src="../{{$user->image}}" style="width:70px;height:70px;">
+        <br>
+        <h2>{{$user->name}}</h2> 
     </div>
     <div class="panel-body"> 
         @if (Auth::guest())                                  <!--  Cheacks fr guest or the owner of profile show delete option if owner  --> 
@@ -21,6 +30,8 @@
             @endif
         @endif    
     </div>
+    
+    
     @if($friends)                         <!--  loop  through friends   -->
         <?php $notfriend = 0 ; ?>        <!-- A varible I used to help prevent an add friend button up showing when users had multiple friends  -->
         @foreach($friends as $friend)
@@ -35,6 +46,8 @@
                                 <input type="submit" value="Unfriend" class="btn btn-default">
                             </form>
                         </div>
+                    
+                    @elseif ($user->id == Auth::user()->id)
                         
                     @elseif ($friend->userb == $user->id && $friend->usera == Auth::user()->id ) <!-- checking for friendship if found shows unfriend button just reversing the positions  -->
                         <div class="panel-body">
@@ -53,7 +66,7 @@
                                 {{csrf_field()}}
                                 <input type='hidden' name="id" value="{{Auth::user()->id}}">
                                 <input type='hidden' name="friendid" value="{{$user->id}}">
-                                <input type="submit" value="Add Friend" class="btn btn-default">
+                                <input type="submit" value="Add Friend." class="btn btn-default">
                             </form>
                         </div>
                     @else
@@ -97,60 +110,107 @@
             @endif
         @endif
     @endif
+@endif
 @endsection
 
 @section('content')
 
+@if (Auth::guest()) 
+    Post's only viewable to logged in user's and friends.
+@else
 <!-- Check for Friends if friend Show Posts if not Do not show posts -->
 
-@if($friends)                         <!--  loop  through friends   -->
-    <?php $notfriend = 0 ; ?>        <!-- A varible I used to help prevent an add friend button up showing when users had multiple friends  -->
-    @foreach($friends as $friend)
-        @if ($friend->usera == $user->id && $friend->userb == Auth::user()->id )  <!-- checking for friendship -->
-        
-            @if($posts)
-                                                     <!-- Loops through posts  --> 
-                @foreach($posts as $post)
-                    <div class="panel panel-default">
+    @if (Auth::user()->id == $user->id)
+                
+        @if($posts)
+                                                             <!-- Loops through posts  --> 
+            @foreach($posts as $post)
+                <div class="panel panel-default">
                         <div class="panel-heading"> 
                             <img src="../{{$post->image}}" style="width:70px;height:70px;"><b>{{$post->name}}</b> 
                         </div>
-                        <div class="panel-body">
-                            <h4>{{$post->title}}</h4> 
-                        </div>
-                        <div class="panel-body">
-                            {{$post->message}} 
-                        </div>
-                        <div class="panel-footer">
-                            Number of comments {{$post->num_com}}<br>
-                            @if (Auth::guest())
-                                <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
-                            @else     
-                                @if (Auth::user()->id == $user->id)
-                                  
-                                    <form method="POST" action="/post/{{$post->id}}">           <!-- if it your post can delete or update --> 
-                                        {{csrf_field()}}
-                                        {{ method_field('DELETE') }}
-                                        <a href="{{url("/post/$user->id")}}" class="btn btn-default">See Comments</a>
-                                        <a href="{{url("/post/$user->id/edit")}}" class="btn btn-default">Update Post</a>
-                                        <input type="submit" value="Delete" class="btn btn-default">
-                                    </form>
-                                @else 
-                                    <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
-                                @endif 
-                            @endif    
-                        </div>
+                    <div class="panel-body">
+                        <h4>{{$post->title}}</h4> 
                     </div>
-                @endforeach
-            @endif
-        @else
-            No Posts Found.
-        @endif 
-    @endforeach
+                    <div class="panel-body">
+                        {{$post->message}} 
+                    </div>
+                    <div class="panel-footer">
+                        Number of comments {{$post->num_com}}<br>
+                        @if (Auth::guest())
+                            <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
+                        @else     
+                            @if (Auth::user()->id == $user->id)
+                                <form method="POST" action="/post/{{$post->id}}">           <!-- if it your post can delete or update --> 
+                                    {{csrf_field()}}
+                                    {{ method_field('DELETE') }}
+                                    <a href="{{url("/post/$user->id")}}" class="btn btn-default">See Comments</a>
+                                    <a href="{{url("/post/$user->id/edit")}}" class="btn btn-default">Update Post</a>
+                                    <input type="submit" value="Delete" class="btn btn-default">
+                                </form>
+                            @else 
+                                <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
+                            @endif 
+                        @endif    
+                    </div>
+                </div>
+            @endforeach
+        @endif          
+    @elseif (Auth::guest()) 
+        No Posts Found..      
     @else
-        Only Friends can view each others post lists.
-@endif 
-            
+    
+        @if($friends)                         <!--  loop  through friends   -->
+            <?php $notfriend = 0 ; ?>        <!-- A varible I used to help prevent an add friend button up showing when users had multiple friends  -->
+            @foreach($friends as $friend)
+                @if ($friend->usera == $user->id && $friend->userb == Auth::user()->id )  <!-- checking for friendship -->
+                
+                    @if($posts)
+                                                             <!-- Loops through posts  --> 
+                        @foreach($posts as $post)
+                            <div class="panel panel-default">
+                                <div class="panel-heading"> 
+                                    <img src="../{{$post->image}}" style="width:70px;height:70px;"><b>{{$post->name}}</b> 
+                                </div>
+                                <div class="panel-body">
+                                    <h4>{{$post->title}}</h4> 
+                                </div>
+                                <div class="panel-body">
+                                    {{$post->message}} 
+                                </div>
+                                <div class="panel-footer">
+                                    Number of comments {{$post->num_com}}<br>
+                                    @if (Auth::guest())
+                                        <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
+                                    @else     
+                                        @if (Auth::user()->id == $user->id)
+                                          
+                                            <form method="POST" action="/post/{{$post->id}}">           <!-- if it your post can delete or update --> 
+                                                {{csrf_field()}}
+                                                {{ method_field('DELETE') }}
+                                                <a href="{{url("/post/$user->id")}}" class="btn btn-default">See Comments</a>
+                                                <a href="{{url("/post/$user->id/edit")}}" class="btn btn-default">Update Post</a>
+                                                <input type="submit" value="Delete" class="btn btn-default">
+                                            </form>
+                                        @else 
+                                            <a href="{{url("/post/$post->id")}}" class="btn btn-default">See Comments</a>
+                                        @endif 
+                                    @endif    
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
+                @elseif (Auth::guest()) 
+                    No Posts Found..
+                @else (Auth::guest()) 
+                    No Posts Found.    
+                @endif 
+            @endforeach
+            @else
+                Only Friends can view each others post lists.
+        @endif 
+    @endif
+@endif
     <div class="panel-footer">
         <a href="http://web-app-dev-paulfirth.c9users.io">Home</a> 
     </div>
